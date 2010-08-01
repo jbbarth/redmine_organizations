b722addc275b8a594a1fa78089c87e9f351942b7
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-01 20:01:29 +0200

    Added tests to ensure Organization tree is sorted correctly

--- test/unit/organization_test.rb
+++ test/unit/organization_test.rb
@@ -3,8 +3,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 class OrganizationTest < ActiveSupport::TestCase
   fixtures :organizations
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_organization_tree_sorting
+    o = Organization.create(:name => "Org A / Team C", :parent_id => 1)
+    assert_equal "Org A / Team B", o.left_sibling.name
+    o.update_attributes(:name => "Org A / Team 0")
+    assert_equal "Org A / Team A", o.right_sibling.name
+    assert_nil o.left_sibling
+    o.update_attributes(:name => "A new org", :parent_id => nil)
+    assert_equal "Org A", o.right_sibling.name
   end
 end
