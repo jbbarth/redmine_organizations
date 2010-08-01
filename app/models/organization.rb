b0f1b635be13734f2add6145e8995324069cd1ac
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-01 19:19:39 +0200

    Keep organization tree sorted by :name

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,14 @@
 class Organization < ActiveRecord::Base
   unloadable
   acts_as_nested_set2
+
+  # Reorder tree after save on the fly
+  # Less beautiful than Redmine method to keep tree sorted,
+  # But also far less complicated
+  after_save do |org|
+    siblings = org.siblings
+    while org.left_sibling && org.left_sibling.name > org.name
+      org.move_left
+    end
+  end
 end
