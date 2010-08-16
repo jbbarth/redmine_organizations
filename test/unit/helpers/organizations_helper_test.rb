46714d30b221acd31dade3aae93d91c07fa23b93
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-16 13:42:30 +0200

    Added a missing unit test for link_to_organization

--- test/unit/helpers/organizations_helper_test.rb
+++ test/unit/helpers/organizations_helper_test.rb
@@ -10,7 +10,9 @@ class OrganizationsHelperTest < HelperTestCase
   test "test rendering of link_to_organization" do
     link_1 = %(<a href="/organizations/1">Org A</a>)
     link_2 = %(<a href="/organizations/2">Team A</a>)
-    assert_equal link_2, link_to_organization(Organization.find(2), :fullname=>false)
-    assert_equal "#{link_1}/#{link_2}", link_to_organization(Organization.find(2))
+    org = Organization.find(2)
+    assert_equal link_2, link_to_organization(org, :fullname=>false)
+    assert_equal "#{link_1}/#{link_2}", link_to_organization(org)
+    assert_equal link_2.gsub("Team A","Org A/Team A"), link_to_organization(org, :link_ancestors=>false)
   end
 end
