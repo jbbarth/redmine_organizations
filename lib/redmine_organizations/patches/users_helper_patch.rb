66f379211a445bb6c629076fe9a73975bcce17d2
jbbarth <jeanbaptiste.barth@gmail.com>
2010-10-12 17:20:49 +0200

    Added 'organizations' tab in user edit view

--- lib/redmine_organizations/patches/users_helper_patch.rb
+++ lib/redmine_organizations/patches/users_helper_patch.rb
@@ -3,6 +3,7 @@ require_dependency 'users_helper'
 module UsersHelper
   def user_settings_tabs
     tabs = [{:name => 'general', :partial => 'users/general', :label => :label_general},
+            {:name => 'organizations', :partial => 'users/organizations', :label => :label_organization_plural},
             {:name => 'memberships', :partial => 'users/memberships', :label => :label_project_plural}
             ]
     if Group.all.any? && Setting["plugin_redmine_organizations"]["hide_groups_admin_menu"] != "1"
