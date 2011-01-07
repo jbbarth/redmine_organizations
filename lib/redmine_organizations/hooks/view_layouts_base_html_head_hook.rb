eb7e0bc8f3bbb7d2a65254fe1daf10846c6a962e
jbbarth <jeanbaptiste.barth@gmail.com>
2011-01-07 08:19:28 +0100

    Moved javascript function into a static .js file, which solves the unability to toggle users/roles when adding the first organization inside a project

--- lib/redmine_organizations/hooks/view_layouts_base_html_head_hook.rb
+++ lib/redmine_organizations/hooks/view_layouts_base_html_head_hook.rb
@@ -2,7 +2,8 @@ module RedmineOrganizations
   module Hooks
     class StylesheetHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context)
-        stylesheet_link_tag "organizations", :plugin => :redmine_organizations
+        stylesheet_link_tag("organizations", :plugin => :redmine_organizations) +
+          javascript_include_tag("organizations", :plugin => :redmine_organizations)
       end
     end
   end
