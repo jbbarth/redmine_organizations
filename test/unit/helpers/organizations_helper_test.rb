529b93d2ce6ceb8091f6d1c0f06a150b20c1401f
jbbarth <jeanbaptiste.barth@gmail.com>
2011-02-14 13:14:59 +0100

    Added ability to force a title in link_to_organization

--- test/unit/helpers/organizations_helper_test.rb
+++ test/unit/helpers/organizations_helper_test.rb
@@ -10,7 +10,7 @@ class OrganizationsHelperTest < HelperTestCase
   test "test rendering of link_to_organization" do
     link_1 = %(<a href="/organizations/1" title="Org A">Org A</a>)
     link_2 = %(<a href="/organizations/2" title="Org A/Team A">Team A</a>)
-    link_3 = %(<a href="/organizations/2">Org A/Team A</a>)
+    link_3 = %(<a href="/organizations/2" title="Org A/Team A">Org A/Team A</a>)
     org = Organization.find(2)
     assert_equal link_2, link_to_organization(org, :fullname=>false)
     assert_equal "#{link_1}/#{link_2}", link_to_organization(org)
