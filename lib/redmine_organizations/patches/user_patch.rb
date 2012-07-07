ac225923d704f82bc156894b2a9b04f783ca1fe0
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-07 14:06:21 +0200

    Removed User#reload, not needed anymore + it breaks some tests in Redmine core

--- lib/redmine_organizations/patches/user_patch.rb
+++ lib/redmine_organizations/patches/user_patch.rb
@@ -14,12 +14,6 @@ class User < Principal
 
   safe_attributes 'organization_id'
   
-  def reload(*args)
-    @name = nil
-    self.memberships.reload
-    super(*args)
-  end
-  
   def roles_through_involvements(project_id, excluded_organization_id)
     m = OrganizationMembership.all(:joins => [:users,:roles],
                                    :conditions => ["organization_memberships.id != ? AND project_id = ? AND users.id = ?",
