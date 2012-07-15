b5a9e5c949ebe9ad6379022c34c7c83aaff34050
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-15 03:46:06 +0200

    Fixed that non-admin users couldn't manage members through organizations

--- app/controllers/organization_memberships_controller.rb
+++ app/controllers/organization_memberships_controller.rb
@@ -1,5 +1,7 @@
 class OrganizationMembershipsController < ApplicationController
-  before_filter :require_admin
+  before_filter :require_login
+  before_filter :find_project_by_project_id, :can_manage_members, :only => [:create_in_project, :update_roles, :update_users, :destroy_in_project]
+  before_filter :require_admin, :only => [:create, :update, :destroy]
 
   helper :organizations
   include OrganizationsHelper   
@@ -100,6 +102,14 @@ class OrganizationMembershipsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to :controller => 'projects', :action => 'settings', :id => membership.project, :tab => 'members' }
       format.js { render(:update) {|page| page.replace_html "tab-content-members", :partial => "projects/settings/members"} }
+    end
+  end
+
+  private
+  def can_manage_members
+    unless User.current.allowed_to?(:manage_members, @project)
+      deny_access
+      return
     end
   end
 end
--- test/functional/organization_memberships_controller_test.rb
+++ test/functional/organization_memberships_controller_test.rb
@@ -1,30 +1,30 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class OrganizationMembershipsControllerTest < ActionController::TestCase
-  def setup
-    @request.session[:user_id] = 1
-  end
-  
   test "should create organization membership" do
+    @request.session[:user_id] = 1
     assert_difference 'Organization.find(1).projects.count', 1 do
-      post :create, :membership => {:organization_id => 1, :project_id => 3, :role_ids => ['1', '2']}
+      post :create, :membership => {:organization_id => 1, :project_id => 5, :role_ids => ['1', '2']}
     end
   end
   
   test "should update organization membership" do
+    @request.session[:user_id] = 1
     put :update, :id => 1, :membership => { :role_ids => ['1', '3']}
     assert_equal [1,3], OrganizationMembership.find(1).role_ids.sort
   end
   
   test "should destroy membership" do
+    @request.session[:user_id] = 1
     assert_difference 'Organization.find(1).projects.count', -1 do
       delete :destroy, :id => 3
     end
   end
 
   test "should destroy membership inside a project" do
+    @request.session[:user_id] = 2
     assert_difference 'Organization.find(1).projects.count', -1 do
-      delete :destroy_in_project, :id => 3
+      delete :destroy_in_project, :project_id => 2, :id => 3
     end
     assert_redirected_to '/projects/onlinestore/settings/members'
   end
