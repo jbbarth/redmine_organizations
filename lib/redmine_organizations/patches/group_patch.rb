f8198e6d34d4c734aa3ef8f38509b327a22fafdb
jbbarth <jeanbaptiste.barth@gmail.com>
2012-12-03 21:12:58 +0100

    Monkey patch Group before it is even loaded from standard dir so that db:migrate work (I want to kill myself now..)

--- lib/redmine_organizations/patches/group_patch.rb
+++ lib/redmine_organizations/patches/group_patch.rb
@@ -1,5 +1,25 @@
 require_dependency 'project' #see: http://www.redmine.org/issues/11035
 require_dependency 'principal'
+
+# It is not possible to "require 'group'" now because at this point,
+# it's possible we don't even have a proper DB. In this case, loading
+# Group scopes is not possible, since ":order" scope is defined without
+# a lambda, so AR tries to find the "users" table layout and fails.
+#
+# So we have to override/fake it and make like we're smarter.
+#
+# Not fun. Will try to propose a patch..
+class Group < Principal
+  def self.order(*args)
+    if table_exists?
+      super
+    else
+      #we don't care, we're in a db:migrate or so..
+      lambda{}
+    end
+  end
+end
+
 require_dependency 'group'
 
 class Group
