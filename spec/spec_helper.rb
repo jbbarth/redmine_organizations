506cf0329526bc74dcb890af1ca65e222d5d651e
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-01 08:33:06 +0200

    Switch to redmine_base_rspec's spec_helper

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-ENV["RAILS_ENV"] ||= 'test'
-
-require File.expand_path('../../../../config/environment', __FILE__)
-
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
-end
