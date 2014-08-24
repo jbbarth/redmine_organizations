0850b28b678ac1545224e2651deb8f63c31432da
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-24 15:35:16 +0200

    Depend on redmine_base_deface and remove dirty hacks for deface integration

--- init.rb
+++ init.rb
@@ -1,12 +1,5 @@
 require 'redmine'
 
-# Little hack for deface in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
-
 # Patches to existing classes/modules
 ActionDispatch::Callbacks.to_prepare do
   #patches
@@ -34,6 +27,7 @@ Redmine::Plugin.register :redmine_organizations do
   version '0.2'
   requires_redmine :version_or_higher => '2.5.0'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
+  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
   settings :default => {
     'hide_groups_admin_menu' => "0",
   }, :partial => 'settings/organizations_settings'
