5df79cdb1229e1c03c8128f43f75cc4aebb981ca
Vincent Robert <vincent.robert@nanego.com>
2015-05-29 13:55:42 +0200

    Update plugin version and redmine lowest compatible version

--- init.rb
+++ init.rb
@@ -25,8 +25,8 @@ Redmine::Plugin.register :redmine_organizations do
   description 'Adds "organization" structure to replace Redmine groups'
   url 'http://github.com/jbbarth/redmine_organizations'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  version '0.4'
-  requires_redmine :version_or_higher => '2.5.0'
+  version '0.5'
+  requires_redmine :version_or_higher => '3.0.0'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
   requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
   settings :default => {
