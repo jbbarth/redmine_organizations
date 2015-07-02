f263cf1dc3b91120703482272c998f9f0799b4c0
Vincent Robert <vincent.robert@nanego.com>
2015-07-02 14:03:48 +0200

    Avoid mass-assignment errors

--- app/models/organization_role.rb
+++ app/models/organization_role.rb
@@ -4,4 +4,6 @@ class OrganizationRole < ActiveRecord::Base
   belongs_to :role
   validates_uniqueness_of :role_id, scope: [:project_id, :organization_id]
   validates_presence_of :role_id, :project_id, :organization_id
+
+  attr_accessible :role_id, :project_id
 end
