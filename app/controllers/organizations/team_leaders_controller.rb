2a7191b14f66e35ac956d5b85c86d55331c7c908
Vincent Robert <vincent.robert@nanego.com>
2019-09-03 10:59:04 +0200

    Add team leaders assignment through organization

--- app/controllers/organizations/team_leaders_controller.rb
+++ app/controllers/organizations/team_leaders_controller.rb
@@ -4,28 +4,37 @@ class Organizations::TeamLeadersController < ApplicationController
 
   def assign_to_team_projects
 
-    @user = User.find(params[:user_id])
-    orga = @user.organization
+    if params[:user_id].present?
+      user = User.find(params[:user_id])
+      users = [user]
+      orga = user.organization
+    else
+      if params[:organization_id].present?
+        orga = Organization.find(params[:organization_id])
+        users = orga.team_leaders
+      else
+        users = []
+      end
+    end
+
     puts "** Mise à jour pour l'organisation #{orga} **"
 
-    projects = orga.self_and_descendants.map {|org| org.projects}.flatten.uniq.compact.select(&:active?)
+    projects = orga.self_and_descendants.map {|org| org.projects}.flatten.uniq.compact.select(&:active?) if orga.present?
     puts "Nombre de projets concernés : #{projects.size}"
 
     project_member = Role.find(4)
     gestionnaire = Role.find(23)
 
-    @message = "#{@user} dispose désormais des rôles lui permettant de gérer son équipe sur tous les projets concernés."
+    @message = "Modification des rôles des utilisateurs : #{users.join(', ')}. Ils disposent désormais des rôles leur permettant de gérer leur équipe sur tous les projets concernés."
 
-    # users = [@user]
-    # users.each do |user|
-      user = @user
+    users.each do |user|
       projects.each do |p|
         member = user.membership(p)
         if member.blank?
           member = Member.new(user: user, project: p)
           member.roles << project_member
           if member.save
-            @message << "\\nAjouté au projet : #{p}"
+            @message << "\\n#{user} ajouté au projet : #{p}"
           else
             @message << "\\n! #{member.errors.messages} | #{p.identifier}"
           end
@@ -34,7 +43,7 @@ class Organizations::TeamLeadersController < ApplicationController
         member.functions << orga.self_and_descendants.map {|org| org.functions_by_project(p)}.flatten.uniq.compact
         member.save
       end
-    # end
+    end
 
     respond_to do |format|
       format.js
