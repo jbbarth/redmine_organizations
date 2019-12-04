adcd45231a0a315ca8a7ad0f2c727dcdfe177346
Vincent Robert <vincent.robert@nanego.com>
2019-12-04 10:36:43 +0100

    Fix access rules

--- lib/redmine_organizations/patches/users_controller_patch.rb
+++ lib/redmine_organizations/patches/users_controller_patch.rb
@@ -3,7 +3,8 @@ require_dependency 'users_controller'
 class UsersController < ApplicationController
 
   skip_before_action :require_admin
-  before_action :require_admin, :except => [:show, :index, :new, :create]
+  before_action :require_admin, :except => [:show, :new, :create]
+  before_action :require_admin_or_manager, :only => [:new, :create]
   after_action :update_memberships_according_to_new_orga, only: [:update]
 
   private
