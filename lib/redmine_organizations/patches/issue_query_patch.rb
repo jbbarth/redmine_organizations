28d85995f8b375776e0ee2068c175c80a5195843
Vincent Robert <vincent.robert@nanego.com>
2021-05-20 17:05:14 +0200

    Add Author Organization filter

--- lib/redmine_organizations/patches/issue_query_patch.rb
+++ lib/redmine_organizations/patches/issue_query_patch.rb
@@ -1,3 +1,5 @@
+require_dependency 'issue_query'
+
 class IssueQuery < Query
   self.available_columns << QueryColumn.new(:author_organization, :groupable => false) if self.available_columns.select { |c| c.name == :author_organization }.empty?
 end
@@ -14,6 +16,52 @@ module PluginOrganizations
         Issue.load_author_organization(issues)
       end
       issues
+    end
+
+    def initialize_available_filters
+      super
+      add_available_filter "author_organization",
+                           :type => :list_optional,
+                           :values => lambda {organization_values},
+                           :label => :field_author_organization
+    end
+
+    def sql_for_author_organization_field(field, operator, value)
+
+      if value.delete('mine')
+        value.push User.current&.organization&.id&.to_s
+      end
+
+      case operator
+      when "*", "!*" # All / None
+        sql_not = operator == "!*" ? 'NOT' : ''
+        "(#{Issue.table_name}.author_id #{sql_not} IN (SELECT DISTINCT #{User.table_name}.id FROM #{User.table_name}" +
+          " WHERE #{User.table_name}.organization_id IS NOT NULL))"
+      when "=", "!"
+        cond = value.any? ?
+                          "#{User.table_name}.organization_id IN (" + value.collect{|val| "'#{self.class.connection.quote_string(val)}'"}.join(",") + ")" :
+                          "1=0"
+        sql_not = operator == "!" ? 'NOT' : ''
+        "(#{Issue.table_name}.author_id #{sql_not} IN (SELECT DISTINCT #{User.table_name}.id FROM #{User.table_name} WHERE #{cond}))"
+      end
+    end
+
+    def organization_values
+      organization_values = []
+      if User.current.logged?
+        organization_values << ["<< #{l(:label_my_organization).downcase} >>", "mine"] if User.current.organization.present?
+      end
+      organization_values += all_organizations_values
+      organization_values
+    end
+
+    def all_organizations_values
+      return @all_organizations_values if @all_organizations_values
+      values = []
+      Organization.sorted.each do |organization|
+        values << [organization.fullname, organization.id.to_s]
+      end
+      @all_organizations_values = values
     end
 
   end
