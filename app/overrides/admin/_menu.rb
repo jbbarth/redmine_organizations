92f06a6869a8ced93bfea47d76e6d0f0feb01171
Vincent Robert <vincent.robert@nanego.com>
2022-06-27 11:54:09 +0200

    Do not override complete partial but insert code at the top

---
+++ app/overrides/admin/_menu.rb
@@ -0,0 +1,4 @@
+Deface::Override.new :virtual_path => "admin/_menu",
+                     :name => "delete-groups-item-menu",
+                     :insert_before => "#admin-menu",
+                     :partial => "admin/delete_groups_item_menu"
