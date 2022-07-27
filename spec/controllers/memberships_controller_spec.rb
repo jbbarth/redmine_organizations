33bbf80ddc21fab8a4ac0219b00826df423cef2a
Julie TILATTI <j.tilatti@adacis.net>
2022-07-27 17:10:53 +0200

    Test correction

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -8,25 +8,32 @@ describe Organizations::MembershipsController, :type => :controller do
            :organization_team_leaders, :members, :member_roles, :roles, :projects
 
   describe 'Admin actions' do
+    let!(:journal_detail_count_before_delation) { JournalDetail.count } if Redmine::Plugin.installed?(:redmine_admin_activity)
+
     before do
       @request.session[:user_id] = 1
     end
 
     it "should delete all members from an organization" do
+
+      user1 = User.find(2)
+      user1.organization = Organization.find(1)
+      user1.save
+      user2 = User.find(3)
+      user2.organization = Organization.find(1)
+      user2.save
+
+      expect do
+        delete :destroy_organization, :params => {
+          :project_id => 1,
+          :id => 1
+        }
+      end.to change { Member.count }.by(-2)
+
       if Redmine::Plugin.installed?(:redmine_admin_activity)
-        user1 = User.find(2)
-        user1.organization = Organization.find(1)
-        user1.save
-        user2 = User.find(3)
-        user2.organization = Organization.find(1)
-        user2.save
-        expect do
-          delete :destroy_organization, :params => {
-            :project_id => 1,
-            :id => 1
-          }
-        end.to change { Member.count }.by(-2)
-           .and change { JournalDetail.where(property: "members").count }.by(2)
+        journal_detail_count_after_delation = JournalDetail.count
+
+        expect(journal_detail_count_after_delation) .to eq(journal_detail_count_before_delation + 4)
 
         last_journals_for_delection_of_member =  JournalDetail.last(4)
         user1_journal = last_journals_for_delection_of_member[0]
@@ -35,37 +42,46 @@ describe Organizations::MembershipsController, :type => :controller do
         user1_name = JSON.parse(user1_journal.old_value)["name"]
         user2_name = JSON.parse(user2_journal.old_value)["name"]
 
-        expect(user1_journal).to have_attributes(:prop_key => "member_roles_and_functions")
-        expect(user2_journal).to have_attributes(:prop_key => "member_roles_and_functions")
+        pro_key_value = Redmine::Plugin.installed?(:redmine_limited_visibility) ?  'member_roles_and_functions' : 'member_with_roles'
+
+        expect(user1_journal).to have_attributes(:prop_key => pro_key_value)
+        expect(user2_journal).to have_attributes(:prop_key => pro_key_value)
         expect(user1_name).to eq(user1.name) .or eq(user2.name)
         expect(user2_name).to eq(user1.name) .or eq(user2.name)
       end
     end
 
     it "should delete all members from an organization except lock one" do
-      if Redmine::Plugin.installed?(:redmine_admin_activity)
-          user1 = User.find(8)
-          user1.organization = Organization.find(1)
-          user1.save
-          user2 = User.find(2)
-          user2.organization = Organization.find(1)
-          user2.save
 
-          expect do
-            delete :destroy_organization, :params => {
-              :project_id => 5,
-              :id => 1
-            }
-          end.to change { Member.count }.by(-1)
-             .and change { JournalDetail.where(property: "members").count }.by(1)
+        user1 = User.find(8)
+        user1.organization = Organization.find(1)
+        user1.save
+        user2 = User.find(2)
+        user2.organization = Organization.find(1)
+        user2.save
+
+        expect do
+          delete :destroy_organization, :params => {
+            :project_id => 5,
+            :id => 1
+          }
+        end.to change { Member.count }.by(-1)
+
+        if Redmine::Plugin.installed?(:redmine_admin_activity)
+          journal_detail_count_after_delation = JournalDetail.count
+
+          expect(journal_detail_count_after_delation) .to eq(journal_detail_count_before_delation + 2)
+
 
           last_journal_for_delection_of_member =  JournalDetail.last(2)[0]
           user_name = JSON.parse(last_journal_for_delection_of_member.old_value)["name"]
 
-          expect(last_journal_for_delection_of_member).to have_attributes(:prop_key => "member_roles_and_functions")
-          expect(user_name).to eq(user2.name)
+          pro_key_value = Redmine::Plugin.installed?(:redmine_limited_visibility) ?  'member_roles_and_functions' : 'member_with_roles'
 
+          expect(last_journal_for_delection_of_member).to have_attributes(:prop_key => pro_key_value)
+          expect(user_name).to eq(user2.name)
+        end
       end
-    end
+
   end
 end
