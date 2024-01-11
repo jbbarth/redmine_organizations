87fe8692f6e0d80447bb715d2148442cce13acdc
Yalaeddin <89978016+Yalaeddin@users.noreply.github.com>
2024-01-11 17:07:37 +0100

    Add a new issue filter: updated by a member of a selected organization

--- lib/redmine_organizations/patches/issue_query_patch.rb
+++ lib/redmine_organizations/patches/issue_query_patch.rb
@@ -24,6 +24,10 @@ module PluginOrganizations
                            :type => :list_optional,
                            :values => lambda {organization_values},
                            :label => :field_author_organization
+      add_available_filter "updated_by_organization",
+                           :type => :list_optional,
+                           :values => lambda {organization_values},
+                           :label => :field_updated_by_organization
     end
 
     def sql_for_author_organization_field(field, operator, value)
@@ -44,6 +48,24 @@ module PluginOrganizations
         sql_not = operator == "!" ? 'NOT' : ''
         "(#{Issue.table_name}.author_id #{sql_not} IN (SELECT DISTINCT #{User.table_name}.id FROM #{User.table_name} WHERE #{cond}))"
       end
+    end
+
+    def sql_for_updated_by_organization_field(field, operator, value)
+      if value.delete('mine')
+        value.push User.current&.organization&.id&.to_s
+      end
+      cond = value.any? ?
+            "#{User.table_name}.organization_id IN (" + value.collect{|val| "'#{self.class.connection.quote_string(val)}'"}.join(",") + ")" :
+            "1=0"
+
+      neg = (operator == '!' ? 'NOT' : '')
+
+      subquery = "SELECT 1 FROM #{Journal.table_name}" +
+      " WHERE #{Journal.table_name}.journalized_type='Issue' AND #{Journal.table_name}.journalized_id=#{Issue.table_name}.id" +
+      " AND (journals.user_id IN (SELECT DISTINCT #{User.table_name}.id FROM #{User.table_name} WHERE #{cond}))" +
+      " AND (#{Journal.visible_notes_condition(User.current, :skip_pre_condition => true)})"
+
+      "#{neg} EXISTS (#{subquery})"
     end
 
     def organization_values
---
+++ spec/models/issue_query_patch_spec.rb
@@ -0,0 +1,97 @@
+require "spec_helper"
+
+describe "IssueQueryPatch" do
+  fixtures :organizations, :users, :roles, :projects, :members, :trackers
+
+  context "filter issues with updated_by_organization" do
+    before do
+      @org = Organization.create(:name => "Team C")
+      @user = User.generate!
+      @user.update_attribute(:organization_id, @org.id)
+    end
+
+    def find_issues_with_query(query)
+      Issue.joins(:status, :tracker, :project, :priority).where(
+        query.statement
+      ).to_a
+    end
+
+    it "Should have updated_by_organization in available_filters" do
+      query = IssueQuery.new(:name => '_')
+      expect(query.available_filters.keys).to include('updated_by_organization')
+    end
+
+    it "operator equal = , one organization" do
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(2), :notes => 'Notes')
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(3), :notes => 'Notes')
+      Journal.create!(:user_id => 2, :journalized => Issue.find(4), :notes => 'Notes')
+
+      query = IssueQuery.new(:name => '_')
+      filter_name = "updated_by_organization"
+
+      query.filters = { filter_name => {:operator => '=', :values => [@org.id.to_s] }}
+
+      expect(find_issues_with_query(query).map(&:id).sort).to include(2, 3)
+    end
+
+    it "operator equal ! , one organization" do
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(2), :notes => 'Notes')
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(3), :notes => 'Notes')
+      Journal.create!(:user_id => 2, :journalized => Issue.find(4), :notes => 'Notes')
+
+      query = IssueQuery.new(:name => '_')
+      filter_name = "updated_by_organization"
+      query.filters = { filter_name => {:operator => '!', :values => [@org.id.to_s] }}
+
+      expect(find_issues_with_query(query).map(&:id).sort).to_not include(2, 3)
+    end
+
+    it "operator equal = , multi organizations" do
+      user_test = User.find(2)
+      org_test = Organization.find(2)
+      user_test.update_attribute(:organization_id, org_test.id)
+
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(2), :notes => 'Notes')
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(3), :notes => 'Notes')
+      Journal.create!(:user_id => user_test.id, :journalized => Issue.find(4), :notes => 'Notes')
+
+      query = IssueQuery.new(:name => '_')
+      filter_name = "updated_by_organization"
+      query.filters = { filter_name => {:operator => '=', :values => [@org.id.to_s, org_test.id.to_s] }}
+
+      expect(find_issues_with_query(query).map(&:id).sort).to include(2, 3, 4)
+    end
+
+    it "operator equal ! , multi organizations" do
+      user_test = User.find(2)
+      org_test = Organization.find(2)
+      user_test.update_attribute(:organization_id, org_test.id)
+
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(2), :notes => 'Notes')
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(3), :notes => 'Notes')
+      Journal.create!(:user_id => user_test.id, :journalized => Issue.find(4), :notes => 'Notes')
+
+      query = IssueQuery.new(:name => '_')
+      filter_name = "updated_by_organization"
+      query.filters = { filter_name => {:operator => '=', :values => [@org.id.to_s, org_test.id.to_s] }}
+
+      expect(find_issues_with_query(query).map(&:id).sort).to_not include([2, 3, 4])
+    end
+
+    it "Should ignore private notes that are not visible" do
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(2), :notes => 'Notes', :private_notes => true)
+      Journal.create!(:user_id => @user.id, :journalized => Issue.find(3), :notes => 'Notes')
+
+      query = IssueQuery.new(:name => '_')
+      filter_name = "updated_by_organization"
+      query.filters = {filter_name => {:operator => '=', :values => [@org.id.to_s]}}
+
+      expect(find_issues_with_query(query).map(&:id).sort).to eq([2, 3])
+
+      User.current =  User.anonymous
+      query.filters = {filter_name => {:operator => '=', :values => [@org.id.to_s]}}
+
+      expect(find_issues_with_query(query).map(&:id).sort).to eq([3])
+    end
+  end
+end
