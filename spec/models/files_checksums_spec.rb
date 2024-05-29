ea7cb03df397b1aa0af129082817a5da785ef23b
Vincent Robert <vincent.robert@nanego.com>
2024-05-29 08:14:38 +0200

    Update checksums in specs to accomodate changes in Redmine 5.1.2

--- spec/models/files_checksums_spec.rb
+++ spec/models/files_checksums_spec.rb
@@ -8,8 +8,8 @@ describe "FilesChecksums" do
   end
 
   it "should ensure project patch file is up to date" do
-    # 5.1.1 & 4.2.9 checksum (the self.allowed_to_condition method is completely overridden and should be reviewed if this test breaks)
-    assert_checksum %w(8c94b6b9b2e7ca2b22387a427441f46f b8ce67efe695bc2381e56db07a0a889e a11cce5462e20a51064f9a72834ac527), "app/models/project.rb"
+    # 5.1.2 & 4.2.9 checksum (the self.allowed_to_condition method is completely overridden and should be reviewed if this test breaks)
+    assert_checksum %w(c31728315eed8b65e2e906c264a1a8ae b8ce67efe695bc2381e56db07a0a889e a11cce5462e20a51064f9a72834ac527), "app/models/project.rb"
   end
 
 end
