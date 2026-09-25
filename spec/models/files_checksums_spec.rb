ef1bebd1bc7807d427d38ebc79a44cfbb81d4eee
Vincent <contact@vincent-robert.com>
2026-09-25 18:31:00 +0200

    Update core project.rb checksums

--- spec/models/files_checksums_spec.rb
+++ spec/models/files_checksums_spec.rb
@@ -8,8 +8,8 @@ describe "FilesChecksums" do
   end
 
   it "ensures the project patch file is up to date" do
-    # 6.1.4 / 7.0.1 / trunk checksums (the self.allowed_to_condition method is completely overridden and should be reviewed if this test breaks)
-    assert_checksum %w(8f1b3c7ec0cc28c13924fb626dc6257d b371ef44053677bba9c927cf47426ec1), "app/models/project.rb"
+    # 6.1.4 / 7.0.1 / 7.0-stable and trunk checksums (the self.allowed_to_condition method is completely overridden and should be reviewed if this test breaks)
+    assert_checksum %w(8f1b3c7ec0cc28c13924fb626dc6257d b371ef44053677bba9c927cf47426ec1 27be4dfb1a6548b605fbb95a38019574), "app/models/project.rb"
   end
 
 end
